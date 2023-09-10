d7cb3a1080fa64e6baff426e94ed46d24235cef7
Aryokonon <arnon.yokoyama@gmail.com>
2023-09-10 17:25:02 +0900

    ajdjusting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
-   root to: 'articles#index'
-  resources :articles
 
- # root to: 'orders#index'
-  resources :orders, only:[:create]
+  # Root route
+  root to: 'items#index' # This sets the root URL of your application to point to the 'index' action of the 'items' controller.
   
+  # Routes for articles
+  get 'articles/index'   # This sets up a route for the 'index' action of the 'articles' controller.
+  get 'articles/new'     # This sets up a route for the 'new' action of the 'articles' controller.
+  resources :articles    # This line generates RESTful routes for the 'articles' resource, including 'index', 'new', 'create', 'edit', 'update', 'show', and 'destroy' actions.
+
+  # Routes for orders
+  resources :orders, only: [:create] # This generates routes for the 'create' action of the 'orders' controller, restricting other actions.
+
+  # You can uncomment the following line if you want the root path to point to the 'orders#index' action.
+  # root to: 'orders#index'
 end
