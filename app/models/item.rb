edcb271229cb45f80c68b660974361bc215188b0
Aryokonon <arnon.yokoyama@gmail.com>
2023-09-24 21:19:25 +0900

    test code

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,14 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_one :order
 
+  # Image presence validation
   validates :image, presence: { message: "can't be blank" }
-  validates :name, presence: { message: "can't be blank" }
-  validates :description, presence: { message: "can't be blank" }
 
+  # Name and description presence validations
+  validates :name, presence: { message: '商品名が必須です' }
+  validates :description, presence: { message: '商品の説明が必須です' }
+
+  # Associations with ActiveHash
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :category, class_name: 'Category'
@@ -15,21 +19,24 @@ class Item < ApplicationRecord
   belongs_to :prefecture, class_name: 'Prefecture'
   belongs_to :shipping_day, class_name: 'ShippingDay', foreign_key: 'shipping_day_id'
 
-  validates :category_id, presence: { message: "can't be blank" }
-  validates :condition_id, presence: { message: "can't be blank" }
-  validates :delivery_fee_id, presence: { message: "can't be blank" }
-  validates :prefecture_id, presence: { message: "can't be blank" }
-  validates :shipping_day_id, presence: { message: "can't be blank" }
+  # Validations for association presence
+  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id,
+            presence: { message: "can't be blank" }
 
-  validates :category_id, numericality: { other_than: 1, message: 'must be selected' }
-  validates :condition_id, numericality: { other_than: 1, message: 'must be selected' }
-  validates :delivery_fee_id, numericality: { other_than: 1, message: 'must be selected' }
-  validates :prefecture_id, numericality: { other_than: 1, message: 'must be selected' }
-  validates :shipping_day_id, numericality: { other_than: 1, message: 'must be selected' }
+  # Validations for association numericality
+  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id,
+            numericality: { other_than: 1, message: 'must be selected' }
 
-  validates :price, presence: { message: "can't be blank" },
-                    numericality: { message: 'is invalid. Input half-width characters',
-                                    only_integer: true }, inclusion: { in: 300..9_999_999, message: 'is out of setting range' }
+  # Price validations
+  validates :price, presence: { message: '価格が必須です' },
+                    numericality: {
+                      only_integer: true,
+                      message: 'は半角数字で入力してください'
+                    },
+                    inclusion: {
+                      in: 300..9_999_999,
+                      message: 'は¥300以上、¥9,999,999以下で入力してください'
+                    }
 
   def sold_out?
     order.present?
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,121 +1,93 @@
-# require 'rails_helper'
-
-# RSpec.describe Item, type: :model do
-#   describe 'Associations' do
-#     it { is_expected.to belong_to(:user) }
-#     it { is_expected.to have_one(:order) }
-#     it { is_expected.to belong_to(:category) }
-#     it { is_expected.to belong_to(:condition) }
-#     it { is_expected.to belong_to(:delivery_fee) }
-#     it { is_expected.to belong_to(:prefecture) }
-#     it { is_expected.to belong_to(:shipping_day) }
-#   end
-
-#   describe 'Validations' do
-#     subject { FactoryBot.build(:item) }
-
-#     it { is_expected.to validate_presence_of(:name).with_message('を入力してください') }
-#     it { is_expected.to validate_presence_of(:description).with_message('を入力してください') }
-#     it { is_expected.to validate_presence_of(:price).with_message('を入力してください') }
-
-#     it 'is invalid without an image' do
-#       subject.image = nil
-#       is_expected.to_not be_valid
-#     end
-
-#     it { is_expected.to validate_numericality_of(:price).only_integer }
-#     it do
-#       is_expected.to validate_inclusion_of(:price)
-#         .in_range(300..9_999_999)
-#         .with_message('は設定範囲外です')
-#     end
-
-#     it 'is invalid with incorrect category_id value' do
-#       subject.category_id = 1
-#       is_expected.to_not be_valid
-#       expect(subject.errors[:category_id]).to include('を選択してください')
-#     end
-
-#     # Include other similar tests for condition_id, delivery_fee_id, prefecture_id, shipping_day_id
-#   end
-
-#   describe '#sold_out?' do
-#     let(:user) { FactoryBot.create(:user) }
-#     let(:item) { FactoryBot.create(:item, user: user) }
-#     let(:order) { FactoryBot.create(:order, item: item) }
-
-#     it 'returns true when the item is sold out' do
-#       expect(item.sold_out?).to eq true
-#     end
-
-#     it 'returns false when the item is not sold out' do
-#       item.order = nil
-#       expect(item.sold_out?).to eq false
-#     end
-#   end
-
-#   describe 'Item Listing' do
-#     before do
-#       user = FactoryBot.create(:user)
-#       @item = FactoryBot.build(:item, user: user)
-#     end
-
-#     context 'when item can be listed' do
-#       it 'has all the required information' do
-#         expect(@item).to be_valid
-#       end
-#     end
-
-#     context 'when item cannot be listed' do
-#       it 'does not have attached image' do
-#         @item.image = nil
-#         @item.valid?
-#         expect(@item.errors.full_messages).to include('画像が必須です')
-#       end
-
-#       it 'has an empty name' do
-#         @item.name = ''
-#         @item.valid?
-#         expect(@item.errors.full_messages).to include('商品名が必須です')
-#       end
-
-#       it 'has an empty description' do
-#         @item.description = ''
-#         @item.valid?
-#         expect(@item.errors.full_messages).to include('商品の説明が必須です')
-#       end
-
-#       it 'has an incorrect category_id value' do
-#         @item.category_id = 1
-#         @item.valid?
-#         expect(@item.errors[:category_id]).to include('を選択してください')
-#       end
-
-#       it 'has an empty price' do
-#         @item.price = ''
-#         @item.valid?
-#         expect(@item.errors.full_messages).to include('価格が必須です')
-#       end
-
-#       it 'has a price less than ¥300' do
-#         @item.price = 299
-#         @item.valid?
-#         expect(@item.errors.full_messages).to include('価格は¥300〜¥9,999,999の間で入力してください')
-#       end
-#     end
-#   end
-# end
-
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before(:each) do
-    @item = FactoryBot.build(:item) # Create a User instance using the factory before each example
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user: @user)
+
+    # Create and attach a sample image to @item
+    image_blob = FactoryBot.create(:active_storage_blob)
+    @item.image.attach(image_blob)
   end
 
-  context 'ユーザ登録が成功する場合' do
-    it '正しい属性であれば有効であること' do
+  describe 'アイテム登録が成功する場合' do
+    it '正しい属性と画像が添付されていれば有効であること' do
       expect(@item).to be_valid
+    end
+  end
+
+  describe 'アイテム登録が失敗する場合' do
+    it '画像が添付されていなければ無効であること' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it '名前が空であれば無効であること' do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Name 商品名が必須です')
+    end
+
+    it '説明が空であれば無効であること' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Description 商品の説明が必須です')
+    end
+
+    it '価格が空であれば無効であること' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price 価格が必須です')
+    end
+
+    it '価格が¥300未満であれば無効であること' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price は¥300以上、¥9,999,999以下で入力してください')
+    end
+
+    it 'category_idの値が不正であれば無効であること' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors[:category_id]).to include('must be selected')
+    end
+
+    it 'condition_idの値が不正であれば無効であること' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors[:condition_id]).to include('must be selected')
+    end
+
+    it 'delivery_fee_idの値が不正であれば無効であること' do
+      @item.delivery_fee_id = 1
+      @item.valid?
+      expect(@item.errors[:delivery_fee_id]).to include('must be selected')
+    end
+
+    it 'prefecture_idの値が不正であれば無効であること' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors[:prefecture_id]).to include('must be selected')
+    end
+
+    it 'shipping_day_idの値が不正であれば無効であること' do
+      @item.shipping_day_id = 1
+      @item.valid?
+      expect(@item.errors[:shipping_day_id]).to include('must be selected')
+    end
+  end
+
+  describe '#売り切れ?' do
+    let(:order) { FactoryBot.create(:order, item: @item) }
+
+    it 'アイテムが売り切れの場合、trueを返すこと' do
+      expect(@item.sold_out?).to eq false
+    end
+
+    it 'アイテムが売り切れでない場合、falseを返すこと' do
+      @item.order = nil
+      expect(@item.sold_out?).to eq false
     end
   end
 end
