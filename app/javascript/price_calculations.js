0e27f8dce0d2058b6d4192da639f397e4508253d
Aryokonon <arnon.yokoyama@gmail.com>
2023-10-15 21:54:30 +0900

    corrections

--- app/javascript/price_calculations.js
+++ app/javascript/price_calculations.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const profit = document.getElementById('profit');
 
   priceInput.addEventListener('input', () => {
-    const inputValue = priceInput.value;
+    const inputValue = Number(priceInput.value); // Explicit conversion to Number
     
-    if(inputValue >= 300 && inputValue <= 9999999) {
-      addTaxPrice.textContent = Math.floor(inputValue * 0.1); // 10% fee
-      profit.textContent = Math.floor(inputValue * 0.9); // 90% profit
+    if (inputValue >= 300 && inputValue <= 9999999) {
+      const tax = Math.floor(inputValue * 0.1); // 10% fee
+      const earnings = inputValue - tax; // Adjusted Profit
+      addTaxPrice.textContent = tax;
+      profit.textContent = earnings;
     } else {
       addTaxPrice.textContent = '---';
       profit.textContent = '---';
